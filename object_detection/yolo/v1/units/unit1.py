4ebb77e8e71f67f91102dce574d9f43c2a65d967
Masum Billal <proafxin@users.noreply.github.com>
2023-11-14 08:17:49 +0600

    changed settings to hyperparameters

--- object_detection/yolo/v1/units/unit1.py
+++ object_detection/yolo/v1/units/unit1.py
@@ -1,11 +1,15 @@
 """First unit of YOLOv1 model"""
 
 from object_detection.yolo.config import LayerConfiguration, MaxPool2dConfiguration
-from object_detection.yolo.v1.settings import IN_CHANNELS
+from object_detection.yolo.v1.hyperparams import hyperparams
 from object_detection.yolo.v1.units.blocks import convolution_block
 
 block1 = convolution_block(
-    kernel_size=7, padding=3, in_channels=IN_CHANNELS, out_channels=64, stride=2
+    kernel_size=7,
+    padding=3,
+    in_channels=hyperparams.in_channels,
+    out_channels=64,
+    stride=2,
 )
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,9 @@
 import pytest
 from torch import Tensor, randn
 
-from object_detection.yolo.v1.settings import IMAGE_SIZE
+from object_detection.yolo.v1.hyperparams import hyperparams
 
 
 @pytest.fixture(scope="function")
 def x_cnn() -> Tensor:
-    return randn(size=(10, 3, IMAGE_SIZE, IMAGE_SIZE))
+    return randn(size=(10, 3, hyperparams.image_size, hyperparams.image_size))
