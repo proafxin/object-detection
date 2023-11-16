3c33e76f32993588d7c75b877926dfdeb91916a1
Masum Billal <proafxin@users.noreply.github.com>
2023-11-17 05:04:11 +0600

    fixed hyperparameters

--- tests/yolo/v1/test_architecture.py
+++ tests/yolo/v1/test_architecture.py
@@ -3,14 +3,22 @@
 from torch import Size, Tensor
 from torch.nn import Module
 
+from object_detection.yolo.v1.hyperparams import hyperparams
 from object_detection.yolo.v1.model import YOLOv1
 
 
-def test_yolov1_architecture(x_cnn: Tensor) -> None:
+def test_yolov1_architecture(batch_size: int, x_cnn: Tensor) -> None:
     yolo = YOLOv1()
     assert isinstance(yolo, Module)
 
     out = yolo(x_cnn)
 
     assert isinstance(out, Tensor)
-    assert out.shape == Size([10, 1470])
+    assert out.shape == Size(
+        [
+            batch_size,
+            (hyperparams.num_boxes * 5 + hyperparams.num_class)
+            * hyperparams.grid_size
+            * hyperparams.grid_size,
+        ]
+    )
